f22e92f86f6143e32e652fba5ef40ab116fc7f4a
camelFace7122 <sanity.7122@gmail.com>
2020-11-03 20:07:19 +0500

    Updates

---
+++ precache-manifest.602c8b29e37abd91d9e91e6f97deafda.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f719a5c79390a83a6d8bcd5129add87a",
+    "url": "/camelFaceNetwork/index.html"
+  },
+  {
+    "revision": "f596ea3aaece8434e15c",
+    "url": "/camelFaceNetwork/static/css/4.22897e1a.chunk.css"
+  },
+  {
+    "revision": "8d4b995189474ca660e8",
+    "url": "/camelFaceNetwork/static/css/5.8dc26e3d.chunk.css"
+  },
+  {
+    "revision": "8a8cc3e7c4f2f3465440",
+    "url": "/camelFaceNetwork/static/css/6.5eee54fc.chunk.css"
+  },
+  {
+    "revision": "63e3ee9ab7b787020df6",
+    "url": "/camelFaceNetwork/static/css/7.8d5803fd.chunk.css"
+  },
+  {
+    "revision": "6d7960f8097d6264b98f",
+    "url": "/camelFaceNetwork/static/css/main.08c6fc69.chunk.css"
+  },
+  {
+    "revision": "b41db0aeeff3a1499a4d",
+    "url": "/camelFaceNetwork/static/js/0.0675efe4.chunk.js"
+  },
+  {
+    "revision": "3ef0d244133c21eb44fa",
+    "url": "/camelFaceNetwork/static/js/3.352a804f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/camelFaceNetwork/static/js/3.352a804f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f596ea3aaece8434e15c",
+    "url": "/camelFaceNetwork/static/js/4.a0e90610.chunk.js"
+  },
+  {
+    "revision": "8d4b995189474ca660e8",
+    "url": "/camelFaceNetwork/static/js/5.1c104685.chunk.js"
+  },
+  {
+    "revision": "8a8cc3e7c4f2f3465440",
+    "url": "/camelFaceNetwork/static/js/6.d78d141e.chunk.js"
+  },
+  {
+    "revision": "63e3ee9ab7b787020df6",
+    "url": "/camelFaceNetwork/static/js/7.25d07074.chunk.js"
+  },
+  {
+    "revision": "6d7960f8097d6264b98f",
+    "url": "/camelFaceNetwork/static/js/main.a5560690.chunk.js"
+  },
+  {
+    "revision": "318dcd130cc8b6835855",
+    "url": "/camelFaceNetwork/static/js/runtime-main.34f2789c.js"
+  },
+  {
+    "revision": "2609accb70fcf276361246c8b87c4165",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-300.2609accb.eot"
+  },
+  {
+    "revision": "2f04e2489209dc0983124e471ce91114",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-300.2f04e248.woff2"
+  },
+  {
+    "revision": "7d5d58f8d0287a814d98016917f46b9e",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-300.7d5d58f8.ttf"
+  },
+  {
+    "revision": "ae10794db78a863a6fe63679755547a4",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-300.ae10794d.svg"
+  },
+  {
+    "revision": "e8d909f1a18b580ab3f8eb7f758efacf",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-300.e8d909f1.woff"
+  },
+  {
+    "revision": "59e90f00a5c268939df49ae2efab4c89",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-600.59e90f00.eot"
+  },
+  {
+    "revision": "8dd029ee6ed7bc1bb41e2afed294d9fa",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-600.8dd029ee.ttf"
+  },
+  {
+    "revision": "b902466c5b1c74ba70a06437bfc413b1",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-600.b902466c.svg"
+  },
+  {
+    "revision": "e52022a88369ad7b3f130360481a7f44",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-600.e52022a8.woff2"
+  },
+  {
+    "revision": "f89d5972b20e5e831023b803f2a0b8ad",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-600.f89d5972.woff"
+  },
+  {
+    "revision": "28cbc4b9c3853a3568ec4fa3738ac726",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-regular.28cbc4b9.eot"
+  },
+  {
+    "revision": "4d05b0c6db738738aa4792648812c025",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-regular.4d05b0c6.ttf"
+  },
+  {
+    "revision": "b3c58c87eda5cae9a1e97b722ba6a190",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-regular.b3c58c87.woff2"
+  },
+  {
+    "revision": "ba48e218807fac431cc6d4b2ca512b5c",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-regular.ba48e218.svg"
+  },
+  {
+    "revision": "c46a139a1e0c6e975d4ce77c0dff99f0",
+    "url": "/camelFaceNetwork/static/media/dosis-v18-latin-regular.c46a139a.woff"
+  },
+  {
+    "revision": "f0845b97b0f930775ccdfda6be843ef7",
+    "url": "/camelFaceNetwork/static/media/flame.f0845b97.svg"
+  },
+  {
+    "revision": "dec5a41dd431b4a6d62e857552f2e0a0",
+    "url": "/camelFaceNetwork/static/media/geneva-plain-001-001.dec5a41d.woff2"
+  },
+  {
+    "revision": "14707564c3a7e767c76d0aa5fb2a6585",
+    "url": "/camelFaceNetwork/static/media/preloader.14707564.gif"
+  },
+  {
+    "revision": "3fdc8cd8b7ca8cf6b573780bede40090",
+    "url": "/camelFaceNetwork/static/media/telegrafico.3fdc8cd8.woff"
+  },
+  {
+    "revision": "5de72dafba593a8165cf9aec5d5719da",
+    "url": "/camelFaceNetwork/static/media/user.5de72daf.png"
+  }
+]);
